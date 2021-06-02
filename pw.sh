dffb075f245b382729b55c0ffac56bf87914697f
Paul W. Rankin <pwr@bydasein.com>
2021-06-03 00:31:21 +1000

    pw.sh: move some subcommands to subcommand flag options
    verify	-> -v
    copy	-> -c
    get-*	-> -k

--- pw.sh
+++ pw.sh
@@ -26,7 +26,7 @@ pw_dir="${PW_DIR:-${HOME}/.pw}"
 
 fail() { echo "$1"; exit 1; }
 
-usage() {
+print_usage() {
 	cat <<EOF
 $program  v$version
 usage:
@@ -34,7 +34,7 @@ usage:
     initialize RSA key pair:
       $public_key
       $private_key
-  $program config
+  $program env
     print PW_* variables (passphrase hidden)
   $program ls|list|find [QUERY]
     list entries matching QUERY; without QUERY, list all
@@ -81,7 +81,7 @@ pkey_init() {
 	chmod 0600 "$public_key"
 }
 
-config() {
+print_env() {
 	[ -n "$PW_PASSPHRASE" ] && pass="***" || pass="-"
 	format='%-16s%s\n'
 	printf "$format" PW_PUBLIC_KEY "${PW_PUBLIC_KEY:-${HOME}/.keys/key.pub}"
@@ -159,16 +159,39 @@ encrypt() {
 	echo "Encryption succeeded: $pw_id"
 }
 
+# clip(stdin)
+# copies first line to clipboard
+# returns: 0
+clip() {
+	[ -n "$PW_CLIP" ] || fail "PW_CLIP not set"
+	sed 1q | tr -d \\n | "$PW_CLIP"
+}
+
+# get_field(stdin)
+# returns: field value string
+get_field() {
+	sed -nE "/^${field}:/ s/.+:[ 	]*(.+)/\1/p"
+}
+
 # decrypt(pw_id)
 # returns: decrypted file contents
 decrypt() {
+	while getopts 'vck:' opt
+	do
+		case "$opt" in
+			(v)		verify=1 ;;
+			(c)		clip=1 ;;
+			(k)		field="$OPTARG" ;;
+		esac
+	done
+	shift $(( OPTIND-1 ))
 	pw_id="$1"
 	pw_tar="${pw_id}.tar"
 	[ -f "$private_key" ] || fail "Private key not found"
 	[ -n "$pw_id" ] || fail "Missing argument"
 	[ -f "$pw_tar" ] || fail "$pw_id not found"
 	[ -n "$PW_PASSPHRASE" ] && pkey_pass_args="-passin env:PW_PASSPHRASE"
-	if [ -n "$PW_VERIFY" ]; then
+	if [ "$verify" == 1 ] || [ -n "$PW_VERIFY" ]; then
 		pw_sig="${pw_tar}.sig"
 		[ -f "$pw_sig" ] || fail "$pw_id signature not found"
 		verify "$pw_id"
@@ -180,10 +203,23 @@ decrypt() {
 	key=$(openssl pkeyutl -decrypt -inkey "$private_key" $pkey_pass_args \
 				  < "${pw_workdir}/${pw_key}" ||
 			  fail "Decryption failed: $pw_key")
-	openssl enc -d -pbkdf2 -aes-256-cbc -pass "pass:${key}" \
-			< "${pw_workdir}/${pw_enc}" ||
-		fail "Decryption failed: $pw_enc"
-	unset key
+	return=$(openssl enc -d -pbkdf2 -aes-256-cbc -pass "pass:${key}" \
+					 < "${pw_workdir}/${pw_enc}" ||
+				 fail "Decryption failed: $pw_enc")
+	if [ $? -ne 0 ]; then
+		fail "$return"
+	elif [ -n "$field" ]; then
+		if [ "$clip" == 1 ]; then
+			echo "$return" | get_field | clip
+		else
+			echo "$return" | get_field
+		fi
+	elif [ "$clip" == 1 ]; then
+		echo "$return" | clip
+	else
+		echo "$return"
+	fi
+	unset key return
 	rm -rf "$pw_workdir"
 }
 
@@ -194,45 +230,38 @@ list() {
 	find "$pw_dir" -type f -maxdepth 1 -name "*${1}*.tar" | sed 's/.*\///; s/\.tar$//' | sort
 }
 
-# copy(pw_id)
-# copies first line to clipboard
-# returns: 0
-copy() {
-	pw_id="$1"
-	[ -n "$PW_CLIP" ] || fail "PW_CLIP not set"
-	return=$(decrypt "$pw_id")
-	if [ $? -ne 0 ]; then
-		fail "$return"
-	else
-		echo "$return" | sed 1q | tr -d \\n | "$PW_CLIP"
-		unset return
-	fi
-}
-
-# get_field(get-FIELD, pw_id)
-# returns: field value string
-get_field() {
-	field=$(echo "$1" | cut -d- -f2)
-	pw_id="$2"
-	return=$(decrypt "$pw_id")
-	if [ $? -ne 0 ]; then
-		fail "$return"
-	else
-		echo "$return" | sed -nE "/^${field}:/ s/.+:[ 	]*(.+)/\1/p"
-		unset return
-	fi
-}
+# # totp(pw_id)
+# # returns: TOTP
+# totp() {
+# 	pw_id="$1"
+# 	[ -n "$pw_id" ] || fail "Missing argument"
+# 	[ -f "${pw_id}.tar" ] || fail "$pw_id not found"
+# 	secret=$(get_field "get-totp" "$pw_id")
+# 	[ -n "$secret" ] || fail "Missing TOTP secret: $pw_id"
+# 	hex=$(date +%s | openssl dgst -r -sha1 -hmac "$secret" | sed -E 's/([0-9a-zA-Z]+).*/\1/' | tr a-z A-Z)
+# 	unset secret
+# 	index16=$(printf "$hex" | tail -c 1)
+# 	index=$(echo $(( 16#$index16 )))
+# 	subhex=$(echo "$hex" | cut -c $(( index + 1 ))-$(( index + 7 )))
+# 	echo $(( 16#$subhex % 1000000 ))
+# }
 
 # totp(pw_id)
 # returns: TOTP
 totp() {
+	getopts 'c' copy && shift
 	pw_id="$1"
 	[ -n "$pw_id" ] || fail "Missing argument"
 	[ "$(command -v oathtool)" ] || fail "Command oathtool not found"
 	[ -f "${pw_id}.tar" ] || fail "$pw_id not found"
 	secret=$(get_field "get-totp" "$pw_id")
 	[ -n "$secret" ] || fail "Missing TOTP secret: $pw_id"
-	oathtool --base32 --totp "$secret"
+	if [ "$copy" == c ]; then
+		[ -n "$PW_CLIP" ] || fail "PW_CLIP not set"
+		oathtool --base32 --totp "$secret" | "$PW_CLIP"
+	else
+		oathtool --base32 --totp "$secret"
+	fi
 	unset secret
 }
 
@@ -266,26 +295,25 @@ pkey_passphrase() {
 			fail "Error changing passphrase: $private_key"
 	chmod 0400 "$private_key"
 }
-
+	
 main() {
 	cd "$pw_dir" 2>/dev/null || fail "$pw_dir not found or PW_DIR not set"
 	case "$1" in
+		(version)		echo "$program v$version" ;;
 		(ls|list|find)	shift; list "$@" ;;
 		(add)			shift; encrypt "$@" ;;
-		(show)			shift; decrypt "$@" ;;
-		(cp|copy)		shift; copy "$@" ;;
 		(edit)			shift; edit "$@" ;;
+		(show)			shift; decrypt "$@" ;;
 		(otp)			shift; totp "$@" ;;
 		(sign)			shift; sign "$@" ;;
 		(verify)		shift; verify "$@" ;;
 		(generate)		shift; generate "$@" ;;
-		(get-*)			get_field "$@" ;;
 		(git)			"$@" ;;
 		(init)			pkey_init ;;
 		(passphrase)	pkey_passphrase ;;
 		(config)		config ;;
-		(version)		echo "$program v$version" ;;
-		(*)				usage ;;
+		(?)				usage ;;
+		(*)				decrypt "$@" ;;
 	esac
 }
 
