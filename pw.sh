bd152492037a5c31ac23301b6bc9851b6bc41fab
Paul W. Rankin <pwr@bydasein.com>
2021-08-08 23:31:38 +1000

    pw.sh: fix commenting

--- pw.sh
+++ pw.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#
+
 # ISC License (ISC)
-#
-# Copyright (c) 2021 Paul W. Rankin <pwr@bydasein.com>
-#
+
+# Copyright (c) 2021, Paul W. Rankin <pwr@bydasein.com>
+
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
-#
+
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 # WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -15,7 +15,7 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-#
+
 # This code is a rewrite of pass, copyright (c) 2018-2019 Roman Zolotarev.
 
 program=$(basename "$0")
